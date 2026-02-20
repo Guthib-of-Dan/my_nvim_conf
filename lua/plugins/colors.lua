aa137682ca8c0a919045c1fe928254e0870e2ff5
Guthib-of-Dan <diril656@gmail.com>
2026-02-20 14:29:53 -0800

    Added background transparency (looks great when background is dark (for me at least)

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,15 +1,15 @@
 if vim.env.C ~= '0' then
-return {
-				{
-				"folke/tokyonight.nvim",
-				lazy=false,
-				priority = 1000,
-				opts = {},
-				config = function()
-								vim.cmd[[colorscheme tokyonight-night]]
-				end
-				}
-}
+  return {
+    {
+      "folke/tokyonight.nvim",
+      lazy=false,
+      priority = 1000,
+      config = function()
+        require("tokyonight").setup({ transparent = true })
+        vim.cmd[[colorscheme tokyonight-night]]
+      end
+    }
+  }
 else 
   return {}
 end
